1b9f428e51b719f1b7049be13c503044e305cfd5
Callum Bennett <callum.bennett@enovation.ie>
2017-11-24 20:39:52 -0500

    add coinigy socketcluster connection, improve CoinList

--- index.js
+++ index.js
@@ -1,11 +1,74 @@
 const path = require('path');
 const url = require('url')
-
 const electron = require('electron');
+const socketCluster = require('socketcluster-client');
 
 const { app, BrowserWindow } = electron;
 
 let mainWindow;
+
+var options = {
+  hostname: 'wss://sc-02.coinigy.com/socketcluster/',
+  secure: true,
+  port: 443,
+  rejectUnauthorized: false // Only necessary during debug if using a self-signed certificate
+};
+
+// // Initiate the connection to the server
+// var socket = socketCluster.connect(options);
+//
+// var coinigyCredentials = require('config').coinigyApiKeys;
+//
+// var options = {
+//   hostname  : "sc-02.coinigy.com",
+//   port      : "443",
+//   secure    : "true"
+// };
+//
+// console.log(options);
+// var SCsocket = socketCluster.connect(options);
+//
+//
+// SCsocket.on('connect', function (status) {
+//
+//   console.log(status);
+//
+//   SCsocket.on('error', function (err) {
+//     console.log(err);
+//   });
+//
+//
+//   SCsocket.emit("auth", coinigyCredentials, function (err, token) {
+//
+//     if (!err && token) {
+//
+//       var scChannel = SCsocket.subscribe("TRADE-OK--BTC--CNY");
+//       console.log(scChannel);
+//       scChannel.watch(function (data) {
+//         console.log(data);
+//       });
+//
+//       SCsocket.emit("exchanges", null, function (err, data) {
+//         if (!err) {
+//           console.log(data);
+//         } else {
+//           console.log(err)
+//         }
+//       });
+//
+//
+//       SCsocket.emit("channels", "OK", function (err, data) {
+//         if (!err) {
+//           console.log(data);
+//         } else {
+//           console.log(err)
+//         }
+//       });
+//     } else {
+//       console.log(err)
+//     }
+//   });
+// });
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
--- src/components/renderers/CoinList.js
+++ src/components/renderers/CoinList.js
@@ -1,29 +1,69 @@
 import React, { Component } from 'react';
-import { Container, Icon, Label, Menu, Table } from 'semantic-ui-react';
+import { Container, Table } from 'semantic-ui-react';
 
 class CoinList extends Component {
 
-  render() {
-    const imageBaseUrl = 'https://coinmarketcap.com/static/img/coins/16x16/';
+  colorClass(percent) {
+    let className;
+      if (percent > 0) {
+        className = 'percent-green';
+      } else if (percent > -5) {
+        className = 'percent-yellow';
+      } else {
+        className = 'percent-red';
+      }
+    className += ' center aligned';
+    return className
+  }
 
+  renderHeaderCell(content, classes) {
+    return <Table.HeaderCell className={classes}>{content}</Table.HeaderCell>;
+  }
+
+  render() {
+
+    console.log(this.props.coins);
     return (
       <Container>
-        <Table celled>
+        <Table celled className="ui inverted table">
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Symbol</Table.HeaderCell>
-              <Table.HeaderCell>Coin</Table.HeaderCell>
+              {this.renderHeaderCell('Coin')}
+              {this.renderHeaderCell('Market Cap', 'center aligned')}
+              {this.renderHeaderCell('1hr Change', 'center aligned')}
+              {this.renderHeaderCell('24hr Change', 'center aligned')}
+              {this.renderHeaderCell('1 week Change', 'center aligned')}
+              {this.renderHeaderCell('BTC', 'center aligned')}
+              {this.renderHeaderCell('USD', 'center aligned')}
             </Table.Row>
           </Table.Header>
 
           <Table.Body>
             {
               this.props.coins.map(coin => {
-                const imageUrl = `${imageBaseUrl}${coin.name.toLowerCase()}.png`;
                 return (
                   <Table.Row key={coin.id}>
-                    <Table.Cell> <img src={imageUrl} /> {coin.symbol}</Table.Cell>
-                    <Table.Cell>{coin.name}</Table.Cell>
+                    <Table.Cell>
+                      {coin.name} ({coin.symbol})
+                    </Table.Cell>
+                    <Table.Cell className="center aligned">
+                      ${parseInt(coin.market_cap_usd).toLocaleString()}
+                      </Table.Cell>
+                    <Table.Cell className={this.colorClass(coin.percent_change_1h)}>
+                      {coin.percent_change_1h}%
+                    </Table.Cell>
+                    <Table.Cell className={this.colorClass(coin.percent_change_24h)}>
+                      {coin.percent_change_24h}%
+                    </Table.Cell>
+                    <Table.Cell className={this.colorClass(coin.percent_change_7d)}>
+                      {coin.percent_change_7d}%
+                    </Table.Cell>
+                    <Table.Cell className="center aligned">
+                      {coin.price_btc}
+                      </Table.Cell>
+                    <Table.Cell className="center aligned">
+                      {parseFloat(coin.price_usd).toFixed(2)}
+                      </Table.Cell>
                   </Table.Row>
                 )
               })
